1189b4ef881528000f4d349e93ff6dc51cdf6191
_firmanahmad <firmanahmad0712@gmail.com>
2022-02-13 09:42:01 +0700

    make it better

--- node.js
+++ node.js
@@ -4,13 +4,13 @@ const path = require('path');
 const router = express.Router();
 app.use(express.static("public"));
 
-router.get('/',function(req,res){
+router.get('/',function(req,res)){
   res.sendFile(path.join(__dirname+'/index.html'));
   //__dirname : It will resolve to your project folder.
-});
+};
 
 app.use('/', router);
 
-app.listen(process.env.port || 3000);
+app.listen(process.env.PORT || 3000);
 
 console.log('Running at Port 3000');
